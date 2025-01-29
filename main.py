86cd2567809a3ac456ea7a6f6c0c937613a82679
Bartosz <bartosz.burdalski@gmail.com>
2025-01-29 18:41:00 +0100

    Add content to columns items

--- main.py
+++ main.py
@@ -21,14 +21,20 @@ with col2:
 
 st.write('Lorem Ipsum is simply dummy text of the printing and typesetting industry.')
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 with col3:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image(f"images/{row['image']}")
+        st.write("[Source Core](https://github.com)")
 
 
 with col4:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image(f"images/{row['image']}")
+        st.write("[Source Core](https://github.com)")
